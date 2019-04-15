55d8e733574b18d381ebb953a1031f37b06e903d
kimyeji <merry2kim@gmail.com>
2019-04-15 15:29:52 +0900

    commit

--- Project_yeji/Project_yeji/source.cpp
+++ Project_yeji/Project_yeji/source.cpp
@@ -40,13 +40,34 @@ using namespace std;
 
 #define MAX 52
 
+void solution(int num);
+
 int map[MAX][MAX];
 int N, M;
 int x, y, dir;
 int ret = 0;
 bool flag = 0;
 
-void solution(int cnt) 
+int main()
+{
+	ios::sync_with_stdio(false);
+
+	cin >> N >> M;
+	cin >> y >> x >> dir;
+
+	for (int i = 0; i < N; i++) {
+		for (int j = 0; j < M; j++) {
+			cin >> map[i][j];
+		}
+	}
+
+	solution(0);
+
+	cout << ret;
+
+}
+
+void solution(int num) 
 {
 	if (flag == 1) return;
 
@@ -54,7 +75,7 @@ void solution(int cnt)
 	int nextX = x;
 
 	//네 방향 모두 청소가 이미 되어있거나 벽이면 바라보는 방향을 유지한 채로 한 칸 후진을 하고 2번으로 돌아감    
-	if (cnt == 4 || map[nextY][nextX] == 1) {
+	if (num == 4 || map[nextY][nextX] == 1) {
 		if (dir == 0) nextY++;
 		if (dir == 1) nextX--;
 		if (dir == 3) nextX++;
@@ -98,27 +119,10 @@ void solution(int cnt)
 
 	//왼쪽 방향에 청소할 방향이 없다면 그 방향으로 회전하고 2번으로 돌아감
 	else {
-		solution(cnt + 1);
+		solution(num + 1);
 		return;
 	}
 
 	return;
 }
-int main()
-{
-	ios::sync_with_stdio(false);
 
-	cin >> N >> M;
-	cin >> y >> x >> dir;
-
-	for (int i = 0; i < N; i++) {
-		for (int j = 0; j < M; j++) {
-			cin >> map[i][j];
-		}
-	}
-
-	solution(0);
-
-	cout << ret;
-
-}
