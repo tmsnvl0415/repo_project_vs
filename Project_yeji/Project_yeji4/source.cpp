8cc146250283caa53b694518e0dbc3d28edf3abf
kimyeji <merry2kim@gmail.com>
2019-04-15 15:04:47 +0900

    commit

--- Project_yeji/Project_yeji4/source.cpp
+++ Project_yeji/Project_yeji4/source.cpp
@@ -51,7 +51,8 @@ static int FirstRow[253][10];
 int Row_index = 0;
 int FirstRow_index = 0;
 
-void main() {
+void main() 
+{
 	bool realSolution = false;
 	cin >> n;
 	inputset = (char **)calloc(n, sizeof(char*) * n);
@@ -59,59 +60,77 @@ void main() {
 	middleset = (char **)calloc(n, sizeof(char*) * n);
 	minoutput = (char **)calloc(n, sizeof(char*) * n);
 
-	for (int i = 0; i < n; i++) {
+	for (int i = 0; i < n; i++) 
+	{
 		inputset[i] = (char *)calloc(n, sizeof(char) * n);
 		outputset[i] = (char *)calloc(n, sizeof(char) * n);
 		middleset[i] = (char *)calloc(n, sizeof(char) * n);
 		minoutput[i] = (char *)calloc(n, sizeof(char) * n);
 	}
 
-	for (int i = 0; i < n; i++) {
-		for (int j = 0; j < n; j++) {
+	for (int i = 0; i < n; i++) 
+	{
+		for (int j = 0; j < n; j++) 
+		{
 			outputset[i][j] = '#';
 			middleset[i][j] = '#';
 		}
 	}
-	/* input set, outputset, middleset, minoutput 동적할당
-	outputset, middleset 초기화*/
+	//input set, outputset, middleset, minoutput 동적할당
+	//outputset, middleset 초기화
 
-	if (n <= 0 || n > 10) {
+	if (n <= 0 || n > 10) 
+	{
 		cout << "1이상 10 이하의 수를 입력해주세요"
 			<< endl << "프로그램이 종료됩니다." << endl;
 	}
 	//n이 범위를 넘어가면 프로그램을 종료한다.
 
-	else {
-		for (int i = 0; i < n; i++) {
-			for (int j = 0; j < n; j++) {
+	else 
+	{
+		for (int i = 0; i < n; i++) 
+		{
+			for (int j = 0; j < n; j++) 
+			{
 				cin >> inputset[i][j];
 			}
 		}
 		cout << endl;
 		r = 0;
-		while (r <= n) {
+		while (r <= n) 
+		{
 			Row_index = 0;
-			for (int i = 0; i < 253; i++) {
-				for (int j = 0; j < 10; j++) {
+			for (int i = 0; i < 253; i++) 
+			{
+				for (int j = 0; j < 10; j++) 
+				{
 					FirstRow[i][j] = 0;
 				}
 			}
-			SaveCombination(n, r);			int ncr = nCr(n, r);
-			for (FirstRow_index = 0; FirstRow_index < ncr; FirstRow_index++) {
+			SaveCombination(n, r);			
+			int ncr = nCr(n, r);
+			for (FirstRow_index = 0; FirstRow_index < ncr; FirstRow_index++) 
+			{
 				click = 0;
-				for (int i = 0; i < n; i++) {
-					for (int j = 0; j < n; j++) {
+				for (int i = 0; i < n; i++) 
+				{
+					for (int j = 0; j < n; j++) 
+					{
 						outputset[i][j] = '#';
 						middleset[i][j] = '#';
 					}
 				}
 				what_light_on(0);
-				if (hassolution == true) {
+				if (hassolution == true) 
+				{
 					realSolution = true;
-					if (best_solution > click) {
+					if (best_solution > click) 
+					{
 						best_solution = click;
-						for (int i = 0; i < n; i++) {
-							for (int j = 0; j < n; j++) {
+						for (int i = 0; i < n; i++) 
+						{
+							for (int j = 0; j < n; j++) 
+							{
 								minoutput[i][j] = outputset[i][j];
 							}
 						}
@@ -121,8 +140,10 @@ void main() {
 			}
 			r++;
 		}
-		if (realSolution == true) {
-			for (int i = 0; i < n; i++) {
+		if (realSolution == true) 
+		{
+			for (int i = 0; i < n; i++) 
+			{
 				for (int j = 0; j < n; j++)
 				{
 					cout << minoutput[i][j] << " ";
@@ -131,7 +152,8 @@ void main() {
 			}
 			cout << endl << " best_solution= " << best_solution << endl;
 		}
-		else {
+		else 
+		{
 			cout << "no Solution" << endl;
 		}
 	}
@@ -144,10 +166,12 @@ void main() {
 //backtracking 하여 어떤 불이 켜져야 할지 정하는 
 void what_light_on(int i)
 {
-	if (promising(i)) {
+	if (promising(i)) 
+	{
 		if (i == n)	//has solution
 		{
-			if (promising(i + 1)) {
+			if (promising(i + 1)) 
+			{
 				hassolution = true;
 			}
 			else {
@@ -160,7 +184,8 @@ void what_light_on(int i)
 			what_light_on(i + 1);
 		}
 	}
-	else {
+	else 
+	{
 		hassolution = false;
 	}
 }
@@ -169,9 +194,12 @@ bool promising(int i)
 {
 	bool promising = true;
 	int t = 0;
-	if (i >= 2) {
-		while (promising == true && t < n) {
-			if (inputset[i - 2][t] != middleset[i - 2][t]) {
+	if (i >= 2) 
+	{
+		while (promising == true && t < n) 
+		{
+			if (inputset[i - 2][t] != middleset[i - 2][t]) 
+			{
 				promising = false;
 			}
 			t++;
@@ -180,59 +208,71 @@ bool promising(int i)
 	return promising;
 }
 
-void toggle(char** &target, int i) {
-	if (i == 0) {
-		for (int j = 0; j < r; j++) {
+void toggle(char** &target, int i) 
+{
+	if (i == 0) 
+	{
+		for (int j = 0; j < r; j++) 
+		{
 			toggle(target, 0, FirstRow[FirstRow_index][j]);
 		}
 	}
-	/*--------i가 0이 아닐때, 첫번째 줄이 아닐때--------*/
-	else {
-		for (int j = 0; j < n; j++) {
-			if (inputset[i - 1][j] == 'O') {
-				if (target[i - 1][j] == '#') {
+	//i가 0이 아닐때, 첫번째 줄이 아닐때
+	else 
+	{
+		for (int j = 0; j < n; j++) 
+		{
+			if (inputset[i - 1][j] == 'O') 
+			{
+				if (target[i - 1][j] == '#')
+				{
 					toggle(target, i, j);
 				}
 			}
-			else if (inputset[i - 1][j] == '#') {
-				if (target[i - 1][j] == 'O') {
+			else if (inputset[i - 1][j] == '#') 
+			{
+				if (target[i - 1][j] == 'O') 
+				{
 					toggle(target, i, j);
 				}
 			}
 		}
 	}
-	/*-------------자신의 위의 행과 비교-------------*/
+	//자신의 위의 행과 비교
 }
 
-/*-----------------------------------------------------------------------------
-i, j를 눌렀다고 가정하고 middleset의 상태를 바꿀 수 있게 해준다.
-(상하좌우 toggle)
------------------------------------------------------------------------------*/
-void toggle(char** &target, int i, int j) {
-	if ((i < n) && (j < n) && (i >= 0) && (j >= 0)) {
+//i, j를 눌렀다고 가정하고 middleset의 상태를 바꿀 수 있게 해준다.
+void toggle(char** &target, int i, int j) 
+{
+	if ((i < n) && (j < n) && (i >= 0) && (j >= 0)) 
+	{
 		if (target[i][j] == 'O')
 			target[i][j] = '#';
 		else if (target[i][j] == '#')
 			target[i][j] = 'O';
-		if ((i < n - 1)) {
+		if ((i < n - 1)) 
+		{
 			if (target[i + 1][j] == 'O')
 				target[i + 1][j] = '#';
 			else if (target[i + 1][j] == '#')
 				target[i + 1][j] = 'O';
 		}
-		if (i >= 1) {
+		if (i >= 1) 
+		{
 			if (target[i - 1][j] == 'O')
 				target[i - 1][j] = '#';
 			else if (target[i - 1][j] == '#')
 				target[i - 1][j] = 'O';
 		}
-		if (j >= 1) {
+		if (j >= 1) 
+		{
 			if (target[i][j - 1] == 'O')
 				target[i][j - 1] = '#';
 			else if (target[i][j - 1] == '#')
 				target[i][j - 1] = 'O';
 		}
-		if (j < n - 1) {
+		if (j < n - 1)
+		{
 			if (target[i][j + 1] == 'O')
 				target[i][j + 1] = '#';
 			else if (target[i][j + 1] == '#')
@@ -243,10 +283,11 @@ void toggle(char** &target, int i, int j) {
 	}
 }
 
-/* Combination의 모든 경우를 저장하게 하는 함수*/
+//Combination의 모든 경우를 저장하게 하는 함수
 void SaveCombination(int n, const int r)
 {
-	if (r > 0) {
+	if (r > 0) 
+	{
 		int *data, *arr;
 		data = (int *)calloc(r, sizeof(int) * r);
 		arr = (int *)calloc(r, sizeof(int) * n);
@@ -255,14 +296,14 @@ void SaveCombination(int n, const int r)
 		combinationUtil(arr, data, 0, n - 1, 0, r);
 	}
 }
-/*재귀적으로 Combination의 모든 경우의 수를 저장하는 함수
--> FirstRow누를 경우를 모두 구함						*/
-void combinationUtil(int arr[], int data[], int start, int end,
-	int index, int r)
+//재귀적으로 Combination의 모든 경우의 수를 저장하는 함수					
+void combinationUtil(int arr[], int data[], int start, int end,	int index, int r)
 {
 	// Current combination is ready to be printed, print it
-	if (index == r) {
-		for (int j = 0; j < r; j++) {
+	if (index == r) 
+	{
+		for (int j = 0; j < r; j++) 
+		{
 			FirstRow[Row_index][j] = data[j] - 1;
 		}
 		Row_index++;
@@ -280,7 +321,7 @@ void combinationUtil(int arr[], int data[], int start, int end,
 	}
 }
 
-/* n choose r의 경우의 수*/
+// n choose r의 경우의 수
 int nCr(int n, int r)
 {
 	if (n == r || r == 0) return 1;
